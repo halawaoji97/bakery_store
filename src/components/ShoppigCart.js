229774004a7cd86472f67cae801106638e00820c
halawaoji97 <ozidhalawa97@gmail.com>
2022-02-02 09:42:02 +0700

    add tax

--- src/components/ShoppigCart.js
+++ src/components/ShoppigCart.js
@@ -8,6 +8,7 @@ import {
   getTotalAmount,
   removeFromCart,
 } from '../redux/cartSlice';
+import NumberFormat from '../utils/numberFormat';
 
 const ShoppingCart = () => {
   const cart = useSelector((state) => state.cart);
@@ -28,11 +29,18 @@ const ShoppingCart = () => {
     dispatch(removeFromCart(product));
   };
 
+  const tax = 0.02;
+  const subTotal = cart.cartTotalAmount;
+  const grandTotal = cart.cartTotalAmount * tax + subTotal;
+
   return (
     <section className='container mx-auto font-quicksand tracking-wide'>
       <h3 className='font-bold text-3xl md:text-4xl text-dark-primary text-center mt-20 md:mt-40'>
         Your Cart
       </h3>
+      <p className='text-dark-secondary text-center'>
+        Make sure your order is paid in full
+      </p>
       <div className='grid grid-cols-1 grid-rows-2 md:grid-rows-1 bg-white md:grid-cols-3 mt-8 md:mt-16 container shadow-lg rounded-2xl border-2 mb-44'>
         <div className='p-8 col-span-2'>
           {cart.cartItems?.map((product, index) => (
@@ -77,33 +85,36 @@ const ShoppingCart = () => {
                   size={24}
                 />
               </button>
-              <div className='w-24'>Rp. {product.cartQty * product.price}</div>
+              <div className='w-24'>
+                {NumberFormat(product.cartQty * product.price)}
+              </div>
               <hr />
             </div>
           ))}
         </div>
-        <div className='bg-dark-primary text-white p-8 h-full rounded-2xl'>
-          <h5 className='font-bold text-xl md:text-3xl tracking-wide'>
-            Summary Order
-          </h5>
-          <div className='subtotal'>
+        <div
+          className='bg-dark-primary font-normal text-white p-8 h-full rounded-2xl flex flex-col justify-evenly'
+          style={{ minHeight: 400 }}
+        >
+          <h5 className='font-bold text-xl md:text-3xl mb-2'>Summary Order</h5>
+          <div className='flex justify-between items-center'>
             <p>Subtotal</p>
-            <span>Rp. {cart.cartTotalAmount}</span>
+            <span>{NumberFormat(cart.cartTotalAmount)}</span>
           </div>
-          <div className='shipping'>
+          <div className='flex justify-between items-center'>
             <p>Shipping</p>
             <span>0</span>
           </div>
-          <div className='tax'>
+          <div className='flex justify-between items-center'>
             <p>Tax</p>
-            <span>10%</span>
+            <span>2%</span>
           </div>
           <hr />
-          <div className='mb-12'>
+          <div className='flex justify-between items-center'>
             <p>Total Pay</p>
-            <span>Rp. {cart.total}</span>
+            <span>{NumberFormat(grandTotal)}</span>
           </div>
-          <button className='bg-gradient-to-r from-yellow-primary to-red-velvet transition-all ease-in duration-0 hover:duration-500 hover:bg-dark-primary  hover:text-white text-dark-secondary rounded-full py-3 px-12  font-semibold'>
+          <button className='bg-gradient-to-r from-yellow-primary to-red-velvet transition-all ease-in duration-0 hover:duration-500 hover:bg-dark-primary hover:text-white text-dark-secondary rounded-full py-3 mt-10 justify-self-end px-12 font-semibold'>
             Checkout
           </button>
         </div>
